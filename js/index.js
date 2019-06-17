a9aa9d93d5546fe28c27fdd4ee2a12635a84ec7c
Ryan Wisniewski <ryanwis98@yahoo.com>
2019-06-17 18:32:16 -0400

    finished! This isnt so bad

--- js/index.js
+++ js/index.js
@@ -38,5 +38,86 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+let nav = document.querySelectorAll("a")
+console.log(nav)
+nav[0].textContent = siteContent["nav"]["nav-item-1"]
+nav[1].textContent = siteContent["nav"]["nav-item-2"]
+nav[2].textContent = siteContent["nav"]["nav-item-3"]
+nav[3].textContent = siteContent["nav"]["nav-item-4"]
+nav[4].textContent = siteContent["nav"]["nav-item-5"]
+nav[5].textContent = siteContent["nav"]["nav-item-6"]
+nav[0].style.color = "green"
+nav[1].style.color = "green"
+nav[2].style.color = "green"
+nav[3].style.color = "green"
+nav[4].style.color = "green"
+nav[5].style.color = "green"
+
+let title = document.querySelector('h1')
+title.textContent = siteContent["cta"]["h1"]
+
+let button = document.querySelector('button')
+button.textContent = siteContent["cta"]["button"]
+
+let headers = document.querySelectorAll('h4');
+headers[0].textContent = siteContent['main-content']['features-h4'];
+headers[1].textContent = siteContent['main-content']['about-h4'];
+headers[2].textContent = siteContent['main-content']['services-h4'];
+headers[3].textContent = siteContent['main-content']['product-h4'];
+headers[4].textContent = siteContent['main-content']['vision-h4']
+headers[5].textContent = siteContent['contact']['contact-h4']
+
+
+// let subTitle = document.querySelector('h4')
+// subTitle.textContent = siteContent["main-content"]["features-h4"]
+// subTitle[1].textContent = siteContent["main-content"]["about-h4"]
+// subTitle[2].textContent = siteContent["main-content"]["services-h4"]
+// subTitle[3].textContent = siteContent["main-content"]["vision-h4"]
+
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// let nav = document.getElementsByTagName("a")
+// nav.setAttribute("a", siteContent["nav"]["nav-item-1"])
+
+let header = document.getElementById("cta-img");
+header.setAttribute('src', siteContent["cta"]["img-src"])
+// header.src = "img/header-img.png"
+
+let insideText = document.querySelectorAll("p")
+insideText[0].textContent = siteContent["main-content"]["features-content"]
+insideText[1].textContent = siteContent["main-content"]["about-content"]
+insideText[2].textContent = siteContent["main-content"]["services-content"]
+insideText[3].textContent = siteContent["main-content"]["product-content"]
+insideText[4].textContent = siteContent["main-content"]["vision-content"]
+insideText[5].textContent = siteContent["contact"]["address"]
+insideText[6].textContent = siteContent["contact"]["phone"]
+insideText[7].textContent = siteContent["contact"]["email"]
+insideText[8].textContent = siteContent["footer"]["copyright"]
+
+
+let midImg = document.getElementById("middle-img")
+midImg.setAttribute("src", siteContent["main-content"]["middle-img-src"])
+
+
+// let backNav = document.createElement("a")
+// backNav.textContent ='Back'
+
+// let frontNav = document.createElement('a')
+// frontNav.textContent = 'front'
+
+// const navAdd = document.querySelector ('nav')
+
+// navAdd.preppend(frontNav)
+// navAdd.append(backNav)
+let newElement = document.createElement('a');
+newElement.textContent= 'Home';
+newElement.style.color = 'green'
+
+let newElementTwo = document.createElement('a')
+newElementTwo.textContent = 'Sign Up'
+newElementTwo.style.color = 'green'
+
+const navAdd = document.querySelector('nav');
+navAdd.prepend(newElement);
+navAdd.append(newElementTwo)
